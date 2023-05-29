468ab7ba3ff3baf684a7bbf2e049b015e666d281
AnkathiPriyanka <121013833+AnkathiPriyanka@users.noreply.github.com>
2023-05-29 10:40:49 +0530

    Update bubble-sort.js

--- assests/bubble-sort.js
+++ assests/bubble-sort.js
@@ -1,3 +1,12 @@
+var arr = [/* Your array here */];
+var size = arr.length;
+var delay = 500; // Delay between each step in milliseconds
+var timerInterval; // Variable to hold the interval ID
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function bubbleSort() {
     var i, j;
     await sleep(delay);
@@ -27,8 +36,9 @@ async function bubbleSort() {
         setColor(j, SORTED);
 
         if (!swapped) {
-            // Entire array is sorted, display message
+            // Entire array is sorted, display message and stop timer
             displaySortedMessage();
+            clearInterval(timerInterval);
             return; // Exit the function as the sorting is complete
         }
     }
@@ -39,4 +49,17 @@ async function bubbleSort() {
 function displaySortedMessage() {
     // Change this part to suit your needs, e.g., show a message in the UI
     console.log("The array is sorted.");
+}
+
+function startSorting() {
+    var start = Date.now(); // Get the current timestamp when sorting starts
+    var timerDiv = document.getElementById("timer");
+    timerDiv.innerHTML = "0 seconds"; // Initialize the timer display
+
+    timerInterval = setInterval(function() {
+        var elapsedTime = Math.floor((Date.now() - start) / 1000); // Calculate the elapsed time in seconds
+        timerDiv.innerHTML = elapsedTime + " seconds"; // Update the timer display
+    }, 1000); // Update the timer every second
+
+    bubbleSort(); // Start the sorting process
 }
